ae15cfb731a480288386a631438753ffb6ffe37d
Dmitry Duka <dduka@nvidia.com>
2016-08-05 02:47:00 +0400

    WIP: layer - use dynamic matrices

--- include/layer.hpp
+++ include/layer.hpp
@@ -10,30 +10,47 @@ namespace nn
 {
 	enum class LayerType
 	{
+		kEigenInput,
 		kEigenRegular
 	};
 
-	template<LayerType type, ActivationType activationType, uint32_t unitsInLayer, uint32_t unitsInPreviousLayer>
+	template<LayerType type, ActivationType activationType, uint32_t unitsInLayer, uint32_t unitsInPreviousLayer = 1>
 	class layer { };
 
 #if USE_EIGEN == 1
+	template<ActivationType activationType, uint32_t unitsInLayer>
+	class layer<LayerType::kEigenInput, activationType, unitsInLayer, 1>
+	{
+	public:
+		using MatrixType = Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic>;
+		static const uint32_t UnitsInLayer = unitsInLayer;
+		static MatrixType computeActivations(const MatrixType& output) { return output.unaryExpr(&activation<activationType>); }
+	};
+
 	template<ActivationType activationType, uint32_t unitsInLayer, uint32_t unitsInPreviousLayer>
 	class layer<LayerType::kEigenRegular, activationType, unitsInLayer, unitsInPreviousLayer>
 	{
 	public:
-		using WeightType = Eigen::Matrix<real, unitsInLayer, unitsInPreviousLayer>;
-		using InputType = Eigen::Matrix<real, unitsInPreviousLayer, 1>;
-		using OutputType = Eigen::Matrix<real, unitsInLayer, 1>;
+		using MatrixType = Eigen::Matrix<real, Eigen::Dynamic, Eigen::Dynamic>;
+		static const uint32_t UnitsInLayer = unitsInLayer;
+		static const uint32_t UnitsInPreviousLayer = unitsInPreviousLayer;
 
-		OutputType computeWeightedSum(const InputType& input) { return m_weight * input + m_bias; }
-
-		OutputType computeActivations(const OutputType& output)
+		layer()
 		{
-			return output.unaryExpr(&activation<activationType>);
+			m_weight = MatrixType::Random(unitsInLayer, unitsInPreviousLayer);
+			m_bias = MatrixType::Random(unitsInLayer, 1);
 		}
+
+		void computeWeightedSum(const MatrixType& input) { m_z = m_weight * input + m_bias; }
+		void computeActivations() { m_a = m_z.unaryExpr(&activation<activationType>); }
+
+		const MatrixType& getWeightedSum() const { return m_z; }
+		const MatrixType& getOutput() const { return m_a; }
 	private:
-		WeightType m_weight;
-		OutputType m_bias;
+		MatrixType m_z;
+		MatrixType m_a;
+		MatrixType m_weight;
+		MatrixType m_bias;
 	};
 #endif
 }
--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 
 #include "cost.hpp"
+#include "timing.hpp"
 #include "layer.hpp"
 #include "activations.hpp"
 
 int main()
 {
-	using namespace nn;
-	layer<LayerType::kEigenRegular, ActivationType::kSigmoid, 30, 256> l1;
+	{
+		using namespace nn;
+		using InputLayer = layer<LayerType::kEigenInput, ActivationType::kSigmoid, 256>;
+		using FirstLayer = layer<LayerType::kEigenRegular, ActivationType::kSigmoid, 30, 256>;
+		using SecondLayer = layer<LayerType::kEigenRegular, ActivationType::kSigmoid, 10, 30>;
+		using InputType = InputLayer::MatrixType;
 
+		timing timer;
+		FirstLayer l1;
+		SecondLayer l2;
+		InputType in = InputType::Random(FirstLayer::UnitsInPreviousLayer, 1);
+		l1.computeWeightedSum(InputLayer::computeActivations(in));
+		l1.computeActivations();
+		l2.computeWeightedSum(l1.getOutput());
+		l2.computeActivations();
+	}
+
+	int x;
+	std::cin >> x;
+	return 0;
 }
